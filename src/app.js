2df4c0d1d46302c4a95dfdb0a72fe8755fb2198b
cwesthoward <169073841+cwesthoward@users.noreply.github.com>
2024-06-04 00:19:38 +0000

    Project Ready

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const pronouns = ["my", "the", "our"];
+  const adjs = ["fuzzy", "obsolete", "wretched"];
+  const nouns = ["coder", "racoon", "cat"];
+
+  const domains = [];
+
+  for (const p of pronouns) {
+    for (const a of adjs) {
+      for (const n of nouns) {
+        const d = p + a + n + ".com";
+        domains.push(d);
+      }
+    }
+  }
+
+  console.log(domains);
+  const listItems = domains.join("<br />");
+  document.querySelector("div").innerHTML = listItems;
 };
